8c4b7acdec7a1bf05d5e32750654aacc81adcee0
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:32:46 -0600

    Creación del segundo menú

--- ProyectoProgra01/Program.cs
+++ ProyectoProgra01/Program.cs
@@ -1,6 +1,6 @@
 ﻿//variables globales
 int option = 0;
-
+int tipo;
 MenuPrincipal();
 //inicio del menu
 void MenuPrincipal()
@@ -26,4 +26,23 @@ do
                 break;
         }
     } while (option != 5) ;
+}
+void PedirDatos()
+{
+    Console.WriteLine("Ingrese el tipo de contenido (1.película, 2.serie, 3.documental, 4.evento en vivo):");
+    while (!int.TryParse(Console.ReadLine(), out tipo) || tipo < 1 || tipo > 4)
+    {
+        Console.WriteLine("Error: Ingrese las opciones validas. (1, 2, 3, 4):");
+    }
+    switch (tipo)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
 }
