b0aa752e7ae88886c5b795d552509e4afe376488
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:29:02 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoProgra01/Program.cs
@@ -0,0 +1,29 @@
+﻿//variables globales
+int option = 0;
+
+MenuPrincipal();
+//inicio del menu
+void MenuPrincipal()
+{
+do
+    {
+        Console.WriteLine("  SIMULADOR DE DECISIONES PARA PLATAFORMA DE STREAMING\n ");
+        Console.WriteLine("             Menú Principal  ");
+        Console.WriteLine("Bienvenido al menú principal");
+        Console.WriteLine("¿sCuál de las opciones desea elejir?");
+        Console.WriteLine("\n1.Evaluar Contenido\n 2.Politicas\n 3.Estadisticas\n 4.Reiniciar Estadisticas\n 5.Salir.");
+        switch (option)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    } while (option != 5) ;
+}
